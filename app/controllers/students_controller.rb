6ddc5dd1157991b99d84b990410261a81fc8ad7b
Romario Gustave <romagustave10@gmail.com>
2020-09-09 06:13:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -12,8 +12,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @post = Post.new 
-    
+    @post = Post.new
+
   end
 
 end
